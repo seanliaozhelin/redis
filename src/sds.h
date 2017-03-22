8116f8de46acdaf6dae052ed8a1ecf3dbaad23ae
seanliaozhelin <jbfxa@qq.com>
2017-03-22 12:07:01 +0800

    Update sds.h

--- src/sds.h
+++ src/sds.h
@@ -83,7 +83,7 @@ struct __attribute__ ((__packed__)) sdshdr64 {
 #define SDS_HDR(T,s) ((struct sdshdr##T *)((s)-(sizeof(struct sdshdr##T))))
 #define SDS_TYPE_5_LEN(f) ((f)>>SDS_TYPE_BITS)
 
-static inline size_t sdslen(const sds s) {
+static inline size_t sdslen(const sds s) {             //d 
     unsigned char flags = s[-1];
     switch(flags&SDS_TYPE_MASK) {
         case SDS_TYPE_5:
